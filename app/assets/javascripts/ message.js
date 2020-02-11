531cb319e5ad1ae2a86de94157f0cd4a5b0c1eb2
kiyo09 <60495508+kiyo09@users.noreply.github.com>
2020-02-11 21:29:41 +0900

    refactoring2

--- app/assets/javascripts/ message.js
+++ app/assets/javascripts/ message.js
@@ -1,5 +1,4 @@
 $(function(){ 
-  // console.log(last_message_id);
 
   function buildHTML(message) {
     if (message.content && message.image) {
@@ -52,60 +51,6 @@ $(function(){
   };
   return html;
 };
-
-
-
-  // var buildHTML = function(message) {
-  //   if (message.content && message.image) {
-  //     var html =`<div class="message-list" data-message-id=` + message.id + `>` +
-  //       `<div class="message-list__upper">` +
-  //         `<div class="message-list__upper__name">` +
-  //           message.user_name +
-  //         `</div>` +
-  //         `<div class="message-list__upper__date">` +
-  //           message.created_at +
-  //         `</div>` +
-  //       `</div>` +
-  //       `<div class="lower-message">` +
-  //         `<p class="message-list__content">` +              
-  //           message.content +
-  //         `</p>` +
-  //         `<img src= "` + message.image + `" class="lower-message__image" >` +
-  //       `</div>` +
-  //     `</div>`
-  //   } else if (message.content) {
-  //     var html = `<div class="message-list" data-message-id=` + message.id + `>` +
-  //       `<div class="message-list__upper">` +
-  //         `<div class="message-list__upper__name">` +
-  //           message.user_name +
-  //         `</div>` +
-  //         `<div class="message-list__upper__date">` +
-  //           message.created_at +
-  //         `</div>` +
-  //       `</div>` +
-  //       `<div class="lower-message">` +
-  //         `<p class="lower-message__content">` +
-  //           message.content +
-  //         `</p>` +
-  //       `</div>` +
-  //     `</div>`
-  //   } else if (message.image) {
-  //     var html = `<div class="message-list" data-message-id=` + message.id + `>` +
-  //       `<div class="message-list__upper">` +
-  //         `<div class="message-list__upper__name">` +
-  //           message.user_name +
-  //         `</div>` +
-  //         `<div class="message-list__upper__date">` +
-  //           message.created_at +
-  //         `</div>` +
-  //       `</div>` +
-  //       `<div class="lower-message">` +
-  //         `<img src="` + message.image + `" class="lower-message__content" >` +
-  //       `</div>` +
-  //     `</div>`
-  //   };
-  //   return html;
-  // };
 
   $('#new_message').on('submit', function(e){
   e.preventDefault();
